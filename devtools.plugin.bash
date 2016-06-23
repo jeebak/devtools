6c08d3c687f5838c816b38b25dabe6c24d2d036b
JeeBak Kim <jeebak.kim@gmail.com>
2016-06-23 11:45:15 -0700

    Add drupal (8) console wrapper

--- devtools.plugin.bash
+++ devtools.plugin.bash
@@ -10,5 +10,6 @@ source "$PLUGIN_D/share/fzf/bindings.bash"
 # Using aliases to avoid having to manipulate $PATH, for now
 alias artisan="$PLUGIN_D/bin/artisan"
 alias cap="$PLUGIN_D/bin/cap"
+alias drupal="$PLUGIN_D/bin/drupal"
 alias drush="$PLUGIN_D/bin/drush"
 alias wp="$PLUGIN_D/bin/wp"
