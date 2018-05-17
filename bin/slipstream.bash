62c1adbcc5f451221d606c2cd4c8b120f69c6b87
JeeBak Kim <jeebak.kim@gmail.com>
2018-05-17 01:43:27 -0700

    Fix adminer bug

--- bin/slipstream.bash
+++ bin/slipstream.bash
@@ -679,7 +679,7 @@ sudo apachectl -k restart
 sleep 3
 # -- SETUP ADMINER ------------------------------------------------------------
 show_status 'Setting up adminer'
-[[ -d   "$DEST_DIR/adminer/webroot" ]] && mkdir -p  "$DEST_DIR/adminer/webroot"
+[[ ! -d "$DEST_DIR/adminer/webroot" ]] && mkdir  -p "$DEST_DIR/adminer/webroot"
 [[ ! -w "$DEST_DIR/adminer/webroot" ]] && chmod u+w "$DEST_DIR/adminer/webroot"
 latest="$(curl -IkLs https://github.com/vrana/adminer/releases/latest | col -b | grep Location | grep -E -o '[^/]+$')"
 
